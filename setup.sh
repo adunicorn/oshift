64afa2acb636511b7daf3a5c656daaa677a33112
root <emanuele.firmani@gmail.com>
2017-06-02 20:08:19 +0100

    DNS resolves controller, master and 2 minions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
-dnf install -y \
+yum install -y \
         git \
         emacs \
         bind bind-utils \
         ansible
 
+
+cp named.conf /etc/named.conf
+cp forward.adunicorn /etc/named
+cp reverse.adunicorn /etc/named
+
+systemctl enable named
+systemctl start named
+
+firewall-cmd --permanent --add-port=53/tcp
+firewall-cmd --permanent --add-port=53/udp
+firewall-cmd --reload
+
+chgrp named -R /var/named
+chown -v root:named /etc/named.conf
+restorecon -rv /var/named
+restorecon /etc/named.conf
+
+systemctl stop NetworkManager
+cp resolv.conf /etc/resolv.conf
