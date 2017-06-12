9d8617f85919601bdd88826e751a0fb9c62c24f4
Emanuele Firmani <emanuele.firmani@gmail.com>
2017-06-12 15:16:34 +0200

    check-cluster checks if DNS works

--- check-cluster.sh
+++ check-cluster.sh
@@ -1,1 +1,5 @@
-ansible -i myinventory nodes -a 'echo OK'
+ansible -i myinventory nodes -a 'echo Can be reached'
+
+ansible -i myinventory nodes -a 'ping master -c 1'
+
+ansible -i myinventory nodes -a 'ping www.google.com -c 1'
