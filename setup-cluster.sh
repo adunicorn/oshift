aed912072032d41712ff2002bc12128d64db0069
Emanuele Firmani <emanuele.firmani@gmail.com>
2017-06-12 15:30:30 +0200

    Typo in setup-cluster.ch

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -2,7 +2,6 @@ WORKDIR=`mktemp -d`
 git clone git@github.com:openshift/openshift-ansible.git ${WORKDIR}
 
 ansible-playbook \
-#    -vvv \
     -i myinventory \
     ${WORKDIR}/playbooks/byo/config.yml
 
