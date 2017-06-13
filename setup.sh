8d52d36d1bc5b91ad7be5f490110fdd4949d61f2
Arialdo Martini <arialdomartini@gmail.com>
2017-06-13 10:13:59 +0100

    Using controller instead of master

--- setup.sh
+++ setup.sh
@@ -1,15 +1,54 @@
 set -e 
 
+CONTROLLER=192.168.1.60
+MAC=192.168.1.80
 MASTER=192.168.1.81
 MINION1=192.168.1.82
 MINION2=192.168.1.83
-UI=192.168.1.80
+
+CONTROLLER_HOSTNAME=controller.adunicorn.local
+MAC_HOSTNAME=mac.adunicorn.local
+MASTER_HOSTNAME=master.adunicorn.local
+MINION1_HOSTNAME=minion1.adunicorn.local
+MINION2_HOSTNAME=minion2.adunicorn.local
+
+NODES="${MASTER} ${MINION1} ${MINION2}"
+ALL_NODES="${MAC} ${CONTROLLER} ${NODES}"
+NODES_HOSTNAMES="${MASTER_HOSTNAME} ${MINION1_HOSTNAME} ${MINION2_HOSTNAME}"
+ALL_NODES_HOSTNAMES="${MAC_HOSTNAME} ${CONTROLLER_HOSTNAME} ${NODES_HOSTNAMES}"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+
+function check_ping {
+  if ping -c 1 $1 &> /dev/null
+  then
+      echo -e ${GREEN}$1 can be reached${NC}
+  else
+      echo -e ${RED}$1 cannot be reached${NC}
+      exit -1
+  fi
+}
+
+
+function check_ping_all {
+    for node in $1
+    do
+	check_ping ${node}
+    done
+}
+
+echo -e "\n\n**************** Checking that all servers respond"
+
+check_ping_all "${ALL_NODES}"
+
 
 echo -e "\n\n**************** Installing software"
 
 yum install -y \
         epel-release
-
 
 yum install -y \
         git \
@@ -42,30 +81,44 @@ restorecon -rv /var/named
 restorecon /etc/named.conf
 
 systemctl stop NetworkManager
-    echo -e "\n\nSkippping resolv.conf: DNS is statically and manually configured on master"
-#cp resolv.conf /etc/resolv.conf
+cp resolv.conf /etc/resolv.conf
 
 
+echo -e "\n\n**************** Checking DNS works on Controller"
+
+check_ping_all "${ALL_NODES_HOSTNAMES}"
+
+
+echo -e "\n\n**************** Creating root's ssh keys on Controller"
 if [ ! -e "/root/.ssh/id_rsa" ]; then
     ssh-keygen -N "" -f /root/.ssh/id_rsa
 fi
 
+echo -e "\n\n**************** Configuring hostnames and authenticasions on nodes"
 
-ssh-copy-id root@master.adunicorn.local
-
-hostname master.adunicorn.local
-
-
-echo -e "\n\n**************** Configuring hostnames on nodes"
-
-for SERVER in minion1 minion2
+for node in ${NODES_HOSTNAMES}
 do
-    ssh-copy-id root@${SERVER}.adunicorn.local
-    ssh root@${SERVER} "hostname ${SERVER}.adunicorn.local"
-    ssh root@${SERVER} "systemctl stop NetworkManager"
-
-    echo -e "\n\nSkippping resolv.conf: DNS is statically and manually configured on each minion"
-#    scp /etc/resolv.conf root@${SERVER}:/etc/resolv.conf
+    echo -e "\n*** Configuring ${node}"
+    ssh-copy-id root@${node}
+    ssh root@${node} "hostname ${node}"
+    ssh root@${node} "systemctl stop NetworkManager"
+    scp /etc/resolv.conf root@${node}:/etc/resolv.conf
 done
 
-echo "Done. You can run ./setup-cluster.sh now and have a beer"
+echo -e "\n\n**************** Checking that DNS works on all nodes"
+
+
+
+echo -e "\n\n**************** Checking that DNS works on all nodes"
+
+for node in ${NODES_HOSTNAMES}
+do
+    echo -e "\n*** Checking DNS on ${node}"
+    for target in ${ALL_NODES_HOSTNAMES}
+    do
+	ssh root@${node} -a "if ping -c 1 ${target} &> /dev/null; then echo -e '${GREEN}${target} can be reached${NC}'; else echo -e '${RED}${target} cannot be reached${NC}'; fi"
+
+    done
+done
+
+echo -e "${GREEN}Done. You can run ./setup-cluster.sh now and have a beer.${NC}"
