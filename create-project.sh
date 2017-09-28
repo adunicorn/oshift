028f6a36e0b1d7a24842e8a131265f7aa02ad214
Arialdo Martini <arialdomartini@gmail.com>
2017-09-28 15:45:50 +0200

    Creation script downloads and pushes docker images to internal registry

--- create-project.sh
+++ create-project.sh
@@ -6,6 +6,16 @@ oc login ${OPENSHIFT_CONSOLE} -u admin -p admin
 
 oc new-project issuing
 
+docker login -u admin -p $(oc whoami -t) $(minishift openshift registry)
+
+docker pull adunicorn/issuing:latest
+docker tag adunicorn/issuing:latest $(minishift openshift registry)/issuing/issuing:latest
+docker push  $(minishift openshift registry)/issuing/issuing:latest
+
+docker pull luiscoms/openshift-rabbitmq:3.6.5-management
+docker tag luiscoms/openshift-rabbitmq:3.6.5-management $(minishift openshift registry)/issuing/rabbitmq:latest
+docker push  $(minishift openshift registry)/issuing/rabbitmq:latest
+
 #oc create -f openshift-templates/redis-ephemeral-template.json
 
 oc create -f openshift-resources/imagestream-rabbitmq.yml
