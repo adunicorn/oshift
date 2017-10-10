4a0e2d04187fc08111df91baf1a81a20ff65f6df
Arialdo Martini <arialdomartini@gmail.com>
2017-10-10 16:12:52 +0200

    Only creates IssuingService on OS

--- create-project.sh
+++ create-project.sh
@@ -1,9 +1,9 @@
 set -e
 
-eval $(minishift docker-env)
-OPENSHIFT_CONSOLE=https://192.168.64.2:8443
-oc login ${OPENSHIFT_CONSOLE} -u admin -p admin
-docker login -u admin -p $(oc whoami -t) $(minishift openshift registry)
+#eval $(minishift docker-env)
+OPENSHIFT_CONSOLE=master.adunicorn.local:8443
+oc login ${OPENSHIFT_CONSOLE} -u admin -p OriginAdmin
+#docker login -u admin -p $(oc whoami -t) $(minishift openshift registry)
 
 
 PROJECT_NAME=issuing
@@ -13,38 +13,39 @@ oc project issuing
 
 echo "\n\n\n** Pushing images in the registry.."
 
-REGISTRY=$(minishift openshift registry)
-echo "  registry=${REGISTRY}"
+#REGISTRY=$(minishift openshift registry)
+#echo "  registry=${REGISTRY}"
 
-docker login -u admin -p $(oc whoami -t) ${REGISTRY}
+#docker login -u admin -p $(oc whoami -t) ${REGISTRY}
 
-docker pull adunicorn/issuing:latest
-docker tag adunicorn/issuing:latest ${REGISTRY}/issuing/issuing:latest
-docker push  ${REGISTRY}/issuing/issuing:latest
+#docker pull adunicorn/issuing:latest
+#docker tag adunicorn/issuing:latest ${REGISTRY}/issuing/issuing:latest
+#docker push  ${REGISTRY}/issuing/issuing:latest
 
-docker pull luiscoms/openshift-rabbitmq:3.6.5-management
-docker tag luiscoms/openshift-rabbitmq:3.6.5-management ${REGISTRY}/issuing/rabbitmq:latest
-docker push ${REGISTRY}/issuing/rabbitmq:latest
+#docker pull luiscoms/openshift-rabbitmq:3.6.5-management
+#docker tag luiscoms/openshift-rabbitmq:3.6.5-management ${REGISTRY}/issuing/rabbitmq:latest
+#docker push ${REGISTRY}/issuing/rabbitmq:latest
 
 
 echo "\n\n\n** Creating OpenShift resources from exports.."
 oc create -f openshift-templates/redis-ephemeral-template.json
 
-#oc create -f openshift-resources/imagestream-rabbitmq.yml
-#oc create -f openshift-resources/imagestream-issuing.yml
+## Issuing
+oc create -f openshift-resources/issuing-imagestream.yml
+oc create -f openshift-resources/issuing-deployment-config.yml
+oc create -f openshift-resources/issuing-service.yml
+oc create -f openshift-resources/issuing-route.yml
 
-oc create -f openshift-resources/deployment-config-issuing.yml
-oc create -f openshift-resources/deployment-config-rabbitmq.yml
-oc create -f openshift-resources/deployment-config-redis.yml
+## RabbitMQ
+#oc create -f openshift-resources/rabbitmq-imagestream.yml
+#oc create -f openshift-resources/rabbitmq-deployment-config.yml
+#oc create -f openshift-resources/rabbitmq-service.yml
+#oc create -f openshift-resources/rabbitmq-route.yml
 
-oc create -f openshift-resources/service-issuing.yml
-oc create -f openshift-resources/service-rabbitmq.yml
-oc create -f openshift-resources/service-redis.yml
-
-oc create -f openshift-resources/route-rabbitmq.yml
-oc create -f openshift-resources/route-issuing.yml
-
-oc create -f openshift-resources/secret-redis.yml
+## Redis
+#oc create -f openshift-resources/redis-deployment-config.yml
+#oc create -f openshift-resources/redis-service.yml
+#oc create -f openshift-resources/redis-secret.yml
 
 
 echo "\n\n\n** Deploying..."
