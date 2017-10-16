6bb56b26681830238660cbe28e198c8d48d9bf4e
Arialdo Martini <arialdomartini@gmail.com>
2017-10-16 14:32:49 +0200

    A consumer that prints random rows

---
+++ db/postgresql/loader/app/create.sh
@@ -0,0 +1,2 @@
+python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/schema.sql
+python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/values.sql
--- db/postgresql/loader/build.sh
+++ db/postgresql/loader/build.sh
@@ -1,1 +1,2 @@
-docker build -t adunicorn/loader .
+docker build -t 172.30.1.1:5000/issuing/loader .
+docker push 172.30.1.1:5000/issuing/loader
